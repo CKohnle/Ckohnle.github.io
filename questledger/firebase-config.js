3fc92c3798da879383ef9ff287bd706b7f28c18c
Christian Kohnle <99084658+CKohnle@users.noreply.github.com>
2026-07-23 21:24:00 +0200

    Update firebase-config.js

--- questledger/firebase-config.js
+++ questledger/firebase-config.js
@@ -1,12 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBzCogyORUtEw8efrl8KoeAPPuK0mFQzy8",
   authDomain: "task-tracker-83250.firebaseapp.com",
   projectId: "task-tracker-83250",
@@ -15,7 +7,3 @@ const firebaseConfig = {
   appId: "1:657729804454:web:a980f2b9e7ca6108e8fc67",
   measurementId: "G-XLS1KS71VZ"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
