1af97fd349144f5aa9ecba87e28e09274036aad4
Christian Kohnle <99084658+CKohnle@users.noreply.github.com>
2026-07-23 18:16:30 +0200

    Create tracker-config.js

---
+++ tracker-config.js
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBzCogyORUtEw8efrl8KoeAPPuK0mFQzy8",
+  authDomain: "task-tracker-83250.firebaseapp.com",
+  projectId: "task-tracker-83250",
+  storageBucket: "task-tracker-83250.firebasestorage.app",
+  messagingSenderId: "657729804454",
+  appId: "1:657729804454:web:a980f2b9e7ca6108e8fc67",
+  measurementId: "G-XLS1KS71VZ"
+};
+
+export const signifRate = {
+  daily: 0.2,
+  minor: 0.5,
+  major: 1,
+  massive: 2
+};
+
+export const exerciseCapHrs = 10;
